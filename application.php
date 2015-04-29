a1bcf280be2ae632a58eef22de7eb2b911be8cd7
Daniel Hanold <dhanold@dnainfo.com>
2015-04-29 14:24:20 -0400

    Add question asking for gender

--- application.php
+++ application.php
@@ -5,11 +5,13 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Acme\Console\Command\GreetCommand;
 use Acme\Console\Command\GreetManyCommand;
+use Acme\Console\Command\GreetGenderCommand;
 use Acme\Console\Command\TextstyleCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 $application->add(new GreetCommand());
 $application->add(new GreetManyCommand());
+$application->add(new GreetGenderCommand());
 $application->add(new TextstyleCommand());
 $application->run();
---
+++ src/Acme/Console/Command/GreetGenderCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Acme\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+class GreetGenderCommand extends Command {
+  protected function configure() {
+    $this
+      ->setName('demo:greet-gender')
+      ->setDescription('Greet someone')
+      ->addArgument('name', InputArgument::REQUIRED, 'Who do you want to greet?')
+      ->addArgument('last_name', InputArgument::OPTIONAL, 'Your last name')
+      ->addOption('yell', NULL, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters')
+      ->addOption('iterations', null, InputOption::VALUE_REQUIRED, 'How many times should the message be printed', 1);
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    // Ask user for a gender.
+    $helper = $this->getHelper('question');
+    $question = new Question('Please enter the gender (male or female): ', 'male');
+    $gender = $helper->ask($input, $output, $question);
+    $output->writeln('Gender selected by user: ' . $gender);
+    $prefix = ($gender == 'male') ? 'Mr. ' : 'Mrs. ';
+
+    $name = $input->getArgument('name');
+    if ($name) {
+      $text = 'Hello ' . $prefix .  $name;
+
+      // If (optional) last name argument was included, add it.
+      if ($last_name = $input->getArgument('last_name')) {
+        $text .= ' ' . $last_name;
+      }
+    } else {
+      $text = 'Hello';
+    }
+
+    if ($input->getOption('yell')) {
+      $text = strtoupper($text);
+    }
+
+    for ($i = 0; $i < $input->getOption('iterations'); $i++) {
+      $output->writeln($text);
+    }
+  }
+}
